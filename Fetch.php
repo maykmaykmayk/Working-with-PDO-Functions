d9c187abb81eaebaf7df2b4e5fdc787341d6ba7f
Joemike Grisola <167644452+maykmaykmayk@users.noreply.github.com>
2024-10-09 22:21:01 +0800

    Create Fetch.php

---
+++ Fetch.php
@@ -0,0 +1,25 @@
+<?php require_once 'dbConfig.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+
+<?php
+$stmt = $pdo->prepare('SELECT * FROM users');
+if ($stmt->execute()) {
+    echo "<pre>";
+    print_r($stmt->fetch());
+    echo "</pre>";
+} else {
+    echo "Failed to retrieve data.";
+}
+
+?>
+
+</body>
+</html>
